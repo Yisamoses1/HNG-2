c30b33d160af9c38ee3d8f00cd5af93f35625880
Yisamoses1 <yisarasaq2018@gmail.com>
2024-07-09 00:54:56 +0100

    correction for deployment

--- Model/index.js
+++ Model/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const config = require("../config/db").development;
+const pg = require("pg")
 
 const sequelize = new Sequelize(
   config.database,
--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 module.exports = {
   development: {
     database: process.env.DB_NAME,
